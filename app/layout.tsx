dae574d251e060b07b9596aab98f8852305920cb
Teamfreya <152387664+Teamfreya@users.noreply.github.com>
2024-10-14 14:55:02 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "My Photo Gallery",
+  title: "Caroloom",
 };
 
 export default function RootLayout({
